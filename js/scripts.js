6b3d150502a3607406757d5849d5ab3d099e5bc2
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-21 22:44:57 -0700

    adding this remove

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $('li').css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $('li').click(function() {
+      alert('hi');
+    });
   });
 
   $("button#goodbye").click(function() {
