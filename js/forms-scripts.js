e1f77477e4a159c07ff120fdf8593e3a18950ad0
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-23 16:31:33 -0700

    appending values to a form

--- js/forms-scripts.js
+++ js/forms-scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
 
     $("#story").show();
 
