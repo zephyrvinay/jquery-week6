ad160e3fdb59c30f2e1a713171d750fb3bcab4db
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-23 16:21:10 -0700

    adding bootstrap to forms

---
+++ js/forms-scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    $(".person1").append("blah blah");
+    $(".person2").append("blah blah");
+    $(".animal").append("blah blah");
+    $(".exclamation").append("blah blah");
+    $(".verb").append("blah blah");
+    $(".noun").append("blah blah");
+
+    $("#story").show();
+  });
+});
