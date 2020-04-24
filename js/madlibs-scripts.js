16f45b86c51d5c0804dc18a41fe97d9fa7b28623
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-23 18:57:39 -0700

    comment debugger and add console.log

--- js/madlibs-scripts.js
+++ js/madlibs-scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
     blanks.forEach(function(blank) {
+      //debugger;
       var userInput = $("input." + blank).val();
+      console.log(userInput);
       $("." + blank).text(userInput).val();      
     });
 
     $("#story").show();
-    alert('Got to end of form submit!');
+    event.preventDefault();
     
   });
 });
